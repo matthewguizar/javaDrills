f9e21ded351259c83f47081e4f2cca66bfd91364
Matthew Guizar <matthewrguizar@gmail.com>
2022-04-26 13:16:45 -0700

    almost finished

---
+++ src/functions.java
@@ -0,0 +1,42 @@
+public class functions {
+    public static void main(String[] args){
+        helloWorld();
+        greeting(" Valerie");
+        add(8, 2);
+        favColorFinder("blue");
+        thatsOdd(10);
+        bigOrSmall();
+    }
+
+    public static void bigOrSmall(){
+
+    }
+    public static void helloWorld(){
+        System.out.println("Hello world!");
+    }
+    public static void greeting(String name){
+        System.out.println("sup" + name);
+    }
+    public static void add(int num1, int num2){
+        System.out.println(num1 + num2);
+    }
+    public static void favColorFinder(String color){
+        if (color == "red"){
+            System.out.println("red is a great color");
+        } else if (color == "green"){
+            System.out.println("green is a solid favorite color");
+        } else if (color == "black"){
+            System.out.println("so trendy");
+        } else {
+            System.out.println("YOU NEED TO EVALUATE YOUR FAVORITE COLOR CHOICE");
+        }
+
+    }
+    public static void thatsOdd(int num){
+        if (num % 2 == 1){
+            System.out.println("odd foo");
+        } else {
+            System.out.println("even cuh");
+        }
+    }
+}
